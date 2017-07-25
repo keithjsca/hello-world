8c9c1cff1a0b14edb3dcbfcf045d9c43e755789b
keithjsca <30446143+keithjsca@users.noreply.github.com>
2017-07-25 19:46:34 +0000

    uploaded four text files from pi
    crazy_script_1.sh was just recently upgraded for loops

---
+++ crazy_script_1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This a comment
+# list files
+ls
+
+# redirect the output of ls to "Malory"
+ls > Malory
+
+# Print a blank line
+echo " "
+
+# Print "catting Malory"
+echo "catting Malory"
+
+# Print the contents of the file "Malory"
+cat Malory
+
+# Print an integer
+echo "1234"
+
+# Print a word
+echo "Word"
+
+
+# print something
+echo "location"
+
+# print current directory
+pwd
+
---
+++ test_for_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assign the variable y a value of 0
+y=0
+
+# Repeat this outer loop 3 times ( y = 1, 2, 3)
+for y in 1 2 3
+do
+
+    # Repeat this inner loop 3 times
+    for x in "Monday" "Tuesday" "Wednesday"
+    do
+    # Print the variable x
+        echo $x
+    done
+
+    # Print a blank line
+    echo " "
+done
